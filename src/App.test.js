c0c0953a20460f12cd35dae52d6a12812156dbc0
Tina Srivastava <tinasrivastava912@gmail.com>
2019-10-12 14:18:45 +0530

    deleted 1 test case

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,3 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
